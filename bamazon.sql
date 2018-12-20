d354b788ea0722a03acbb9e2bcfefee5984893ed
sfattal <fattal.sherif@gmail.com>
2018-12-20 12:14:36 -0500

    customer schema

---
+++ bamazon.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(5,2) NOT NULL,
+  stock_quantity INT(4) NOT NULL,
+  PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Red Dead Redemption 2 PlayStation 4", "Video Games", "59.88", 10),
+("DualShock 4 Wireless Controller for PlayStation 4 Jet Black", "Video Games", "46.69", 8),
+("Carhartt Mens Acrylic Watch Hat", "Clothing", "9.99", 7),
+("Champion Mens Powerblend Fleece Pullover Hoodie", "Clothing", "23.58", 5),
+("Yankee Candle Large Jar Candle, Midsummers Night", "Home & Kitchen", "17.99", 3),
+("Fleece Blanket Grey Microfiber", "Home & Kitchen", "25.99", 4),
+("Burts Bees Natural Moisturizing Lip Balm 4 Tubes", "Beauty & Personal Care", "6.64", 10),
+("Berocca Orange Flavor Effervescent Tablets 4 Tubes", "Beauty & Personal Care", "7.84", 8),
+("Blue Buffalo Life Protection Formula Natural Adult Dry Dog Food", "Pet Supplies", "29.39", 5),
+("Multipet Duckworth Duck Toy Large", "Pet Supplies", "8.41", 5)
---
+++ bamazonCustomer.js
@@ -0,0 +1,25 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "password",
+    database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    start();
+});
+
+function start() {
+    connection.query("SELECT * FROM products", function(err, results) {
+        
+    })
+}
