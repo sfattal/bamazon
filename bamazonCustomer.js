878d7570d8295d5a466ed7ca45d77ae0298f5be6
sfattal <fattal.sherif@gmail.com>
2018-12-20 14:09:52 -0500

    user response inventory check

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,12 +21,23 @@ connection.connect(function(err) {
 function start() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.log(res)
+        
+        console.log("Found " + res.length + " products")
+        for (i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].item_id + " | Name: " + res[i].product_name + " | Price: " + res[i].price)
+        }
 
         inquirer.prompt([
             {
                 name: "purchaseID",
-                type: "input",
+                type: "rawlist",
+                choices: function() {
+                    var productArray = [];
+                    for (var i = 0; i < res.length; i++) {
+                        productArray.push(res[i].item_id);
+                    }
+                    return productArray;
+                },
                 message: "What is the ID for the product you would like to purchase?",
             },
             {
@@ -35,5 +46,22 @@ function start() {
                 message: "How many units would like to purchase?",
             }
         ])
+        
+        .then(function(answer) {
+            var chosenProduct;
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].item_id === answer.purchaseID) {
+                    chosenProduct = results[i];
+                }
+            
+                if (chosenProduct.stock_quantity > answer.purchaseQty) {
+                    console.log("Order Successfully Placed!" + " Your total is: " + "$" + parseInt(purchaseQty*res[i].price))
+                }
+                else {
+                    console.log("We are sorry, your order could not be placed due to insufficient quantity in stock")
+                }
+            }
+        })
+
     })
 }
